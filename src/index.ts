c2ecdba5fdf01a429596ef6929e175f95ba26127
Vodovva <valerievodovnik@gmail.com>
2024-02-02 10:11:27 -0600

    added get and set

--- src/index.ts
+++ src/index.ts
@@ -44,8 +44,26 @@ class Item {
 
 class User {
     private _id: string = uuidv4();
+    public get id_1(): string {
+        return this._id;
+    }
+    public set id_1(value: string) {
+        this._id = value;
+    }
     private _name: string;
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
     private _age: number;
+    public get age(): number {
+        return this._age;
+    }
+    public set age(value: number) {
+        this._age = value;
+    }
     private _cart: Item[] = [];
 
     constructor(name: string, age: number) {
