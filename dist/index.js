c4c8a533f19d45950f946c1b29b64f1c3db5bad9
Vodovva <valerievodovnik@gmail.com>
2024-02-01 22:17:34 -0600

    complete redo

--- dist/index.js
+++ dist/index.js
@@ -1,77 +1,98 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const uuid_1 = require("uuid");
-// Create new user
-function createUser(name, age) {
-    return { id: (0, uuid_1.v4)(), name, age, cart: [] };
+class Item {
+    constructor(name, price, description) {
+        this._id = (0, uuid_1.v4)();
+        this._name = name;
+        this._price = price;
+        this._description = description;
+    }
+    get id() {
+        return this._id;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get price() {
+        return this._price;
+    }
+    set price(value) {
+        this._price = value;
+    }
+    get description() {
+        return this._description;
+    }
+    set description(value) {
+        this._description = value;
+    }
 }
-// Create new item
-function createItem(name, price, description) {
-    return { id: (0, uuid_1.v4)(), name, price, description };
+class User {
+    constructor(name, age) {
+        this._id = (0, uuid_1.v4)();
+        this._cart = [];
+        this._name = name;
+        this._age = age;
+    }
+
+    get cart() {
+        return [...this._cart];
+    }
+
+    addToCart(item) {
+        this._cart.push(item);
+    }
+
+    removeFromCart(itemId) {
+        this._cart = this._cart.filter(item => item.id !== itemId);
+    }
+
+    removeQuantityFromCart(itemId, quantity) {
+        let count = 0;
+        this._cart = this._cart.filter(item => {
+            if (item.id === itemId && count < quantity) {
+                count++;
+                return false;
+            }
+            return true;
+        });
+    }
+
+    cartTotal() {
+        return this._cart.reduce((total, item) => total + item.price, 0);
+    }
+
+    printCart() {
+        this._cart.forEach(item => console.log(`${item.name}: $${item.price}`));
+        console.log(`Total: $${this.cartTotal()}`);
+    }
 }
-// Add item to cart
-function addToCart(user, item) {
-    user.cart.push(item);
+class Shop {
+    constructor() {
+        this._items = [];
+        this._items.push(new Item('Rose Quartz', 25.00, "Bring more love, harmony and peace"));
+        this._items.push(new Item('Tigers Eye', 25.00, "Offers protection, personal power and determination"));
+        this._items.push(new Item('lapis Lazuli', 20.00, "Welcome wisdom, intuition, and clarity"));
+    }
+    get items() {
+        return this._items;
+    }
 }
-// Remove item from cart
-function removeFromCart(user, itemId) {
-    user.cart = user.cart.filter(item => item.id !== itemId);
-}
-// Remove quantity from cart
-function removeQuantityFromCart(user, itemId, quantity) {
-    let count = 0;
-    user.cart = user.cart.filter(item => {
-        if (item.id === itemId && count < quantity) {
-            count++;
-            return false;
-        }
-        return true;
-    });
-}
-// Cart total
-function cartTotal(user) {
-    return user.cart.reduce((total, item) => total + item.price, 0);
-}
-// Print the cart
-function printCart(user) {
-    let total = 0;
-    user.cart.forEach(item => {
-        console.log(`Item: ${item.name}, Price: ${item.price}`);
-        total += item.price;
-    });
-    console.log(`Total Cart Value: ${total}`);
-}
-// Create a User
-let user = createUser("Alice", 25);
-// Create 3 Items
-let itemA = createItem("Item A", 10, "Description of Item A");
-let itemB = createItem("Item B", 20, "Description of Item B");
-let itemC = createItem("Item C", 30, "Description of Item C");
-// Add Item A to the user's Cart
-addToCart(user, itemA);
-// Print Cart Contents and Total
-printCart(user);
-console.log(`Total: ${cartTotal(user)}`);
-// Add 3 Item B to the user's Cart
-addToCart(user, itemB);
-addToCart(user, itemB);
-addToCart(user, itemB);
-// Print Cart Contents and Total
-printCart(user);
-console.log(`Total: ${cartTotal(user)}`);
-// Add 3 Item C to the user's Cart
-addToCart(user, itemC);
-addToCart(user, itemC);
-addToCart(user, itemC);
-// Print Cart Contents and Total
-printCart(user);
-console.log(`Total: ${cartTotal(user)}`);
-// Remove all of Item B from the cart
-removeFromCart(user, itemB.id);
-// Print Cart Contents and Total
-printCart(user);
-console.log(`Total: ${cartTotal(user)}`);
-// Remove 2 of Item C from the cart
-removeQuantityFromCart(user, itemC.id, 2);
-// Print Cart Contents and Total
-printCart(user);
+
+let shop = new Shop();
+let user = new User("Apollo Jupiter", 30);
+
+user.addToCart(shop.items[0]); 
+user.printCart();
+
+user.removeFromCart(shop.items[0].id);
+user.printCart();
+
+user.addToCart(shop.items[1]);
+user.addToCart(shop.items[1]);
+user.addToCart(shop.items[1]); 
+user.removeQuantityFromCart(shop.items[1].id, 2); 
+user.printCart();
